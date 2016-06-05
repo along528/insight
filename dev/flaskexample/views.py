dc2301408bdf57fe1293b809814795ced983be4f
along528 <along528@gmail.com>
2016-06-05 14:04:31 -0400

    completed flask example

--- dev/flaskexample/views.py
+++ dev/flaskexample/views.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
+from flask import request
+from a_Model import ModelIt
 
 user = 'along528' #add your username here (same as previous postgreSQL)                      
 host = 'localhost'
@@ -30,3 +32,35 @@ def birth_page():
         births += query_results.iloc[i]['birth_month']
         births += "<br>"
     return births
+ 
+@app.route('/db_fancy')
+def cesareans_page_fancy():
+    sql_query = """
+               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
+                """
+    query_results=pd.read_sql_query(sql_query,con)
+    births = []
+    for i in range(0,query_results.shape[0]):
+        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+    return render_template('cesareans.html',births=births)
+ 
+@app.route('/input')
+def cesareans_input():
+    return render_template("input.html")
+
+@app.route('/output')
+def cesareans_output():
+  #pull 'birth_month' from input field and store it
+  patient = request.args.get('birth_month')
+    #just select the Cesareans  from the birth dtabase for the month that the user inputs
+  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
+  print query
+  query_results=pd.read_sql_query(query,con)
+  print query_results
+  births = []
+  for i in range(0,query_results.shape[0]):
+      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+      the_result = ''
+  #return render_template("output.html", births = births, the_result = the_result)
+  the_result = ModelIt(patient,births)
+  return render_template("output.html", births = births, the_result = the_result)
