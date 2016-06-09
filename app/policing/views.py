989fb79464ef7afddb2f91d3dc663cce7818e668
along528 <along528@gmail.com>
2016-06-08 23:07:09 -0400

    built simple app that queries table

--- app/policing/views.py
+++ app/policing/views.py
@@ -14,6 +14,21 @@ db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
 con = None
 con = psycopg2.connect(database = dbname, user = user)
 
+def clean_df(results):
+    results['agency'] = results['agency'].str.title()
+    results['agency'] = results['agency'].str.replace("'S","'s")
+    results['agency'] = results['agency'].str.replace("ffs","ff's")
+    results.columns = results.columns.str.title()
+    columns = results.columns.tolist()
+    columns[columns.index("Rpsi")] = "RPSI"
+    results.columns = columns
+    return results
+def get_html(results):
+    #return results.to_html(index=False, float_format = lambda x: "%3.2f" % (x)).replace("dataframe", "table table-hover")
+    return results.to_html(index=False).replace("dataframe",
+					       "table table-hover")
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -21,34 +36,22 @@ def index():
        title = 'Home', user = { 'nickname': 'Miguel' },
        )
 
-@app.route('/db_fancy')
+@app.route('/db')
 def db_page_fancy():
     sql_query = """
                 SELECT agency,zipcode,year,white,black,rpsi FROM combined_rpsi_searches_over_stops_black_over_white WHERE year = 2006;          
                 """
-    query_results=pd.read_sql_query(sql_query,con)
-    agencies = []
-    for i in range(0,query_results.shape[0]):
-        agencies.append(dict(agency=query_results.iloc[i]['agency'], zipcode=query_results.iloc[i]['zipcode'], rpsi=query_results.iloc[i]['rpsi']))
-    return render_template('rpsi_db.html',agencies=agencies)
+    results=get_html(clean_df(pd.read_sql_query(sql_query,con)))
+    return render_template('rpsi_db.html', policing_db=results)
  
 @app.route('/input')
-def cesareans_input():
+def input():
     return render_template("input.html")
 
 @app.route('/output')
-def cesareans_output():
-  #pull 'birth_month' from input field and store it
-  patient = request.args.get('birth_month')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-  print query
-  query_results=pd.read_sql_query(query,con)
-  print query_results
-  births = []
-  for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-      the_result = ''
-  #return render_template("output.html", births = births, the_result = the_result)
-  the_result = ModelIt(patient,births)
-  return render_template("output.html", births = births, the_result = the_result)
+def output():
+  agency = request.args.get('agency')
+  query = "SELECT agency,zipcode,year,white,black,rpsi FROM combined_rpsi_searches_over_stops_black_over_white WHERE year = 2006 AND (agency LIKE '%"+agency.upper()+"%');" 
+  results=get_html(clean_df(pd.read_sql_query(query,con)))
+  #the_result = ModelIt(patient,births)
+  return render_template("output.html", policing_db = results)
