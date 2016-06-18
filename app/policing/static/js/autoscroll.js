d92dddb96aab9ad78509e4e933ff9c5120d3e0f0
along528 <along528@gmail.com>
2016-06-17 22:21:37 -0400

    updated app with new style!

---
+++ app/policing/static/js/autoscroll.js
@@ -0,0 +1,16 @@
+//$(document).ready( function(){
+//  var scrollToElement = $("#results");
+//    $(window).scrollTop( scrollToElement.offset().top);
+//});
+
+ $('html, body').animate({
+     scrollTop: $("#results").offset().top
+ },1500,'easeInOutExpo');
+
+//$(document).ready( function(){
+ //    var $anchor = $("#results");
+  //      $('html, body').stop().animate({
+   //         scrollTop: $($anchor.attr('href')).offset().top
+    //    }, 1500, 'easeInOutExpo');
+
+//}};
